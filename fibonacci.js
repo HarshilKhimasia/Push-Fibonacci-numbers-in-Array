5ae7db06d930c79b778c736976cfb0356b3f7bc5
Harshil Khimasia <118171733+HarshilKhimasia@users.noreply.github.com>
2024-03-21 07:47:39 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -11,9 +11,33 @@ const createFibonacciArr = (n) => {
       arr.push(sum);
     }
   }
-  console.log(arr);
-  return arr;
+  return arr;   // Returns an array of Fibonacci Numbers till n
 };
+
+const examFun = (arrOFNums) => {
+  let largeNum = 0;
+  for (let i = 0; i < arrOFNums.length; i++) {
+    if (arrOFNums[i] > largeNum) {
+      largeNum = arrOFNums[i];
+    }
+  }
+
+  const fibonacciArr = createFibonacciArr(largeNum); // Largest Number from the array is passes as argument which will create a Fibonacci Series till that Number
+  let statusNum = false;
+  for (let i = 0; i < arrOFNums.length; i++) {
+    for (let x = 0; x < fibonacciArr.length; x++) {
+      if (arrOFNums[i] == fibonacciArr[x]) {                                 // Checks wheter a number belongs to the Series in Fibonacci Series
+        statusNum = !statusNum;
+        console.log(`Number ${arrOFNums[i]} belongs to Fibonacci Series`);  
+      }
+    }
+    if (!statusNum) {
+      console.log(`Number ${arrOFNums[i]} is not a member of the series`); 
+    }
+  }
+};
+const sArr = [6, 7, 22, 48, 120, 13];
+examFun(sArr);
 
 
 
